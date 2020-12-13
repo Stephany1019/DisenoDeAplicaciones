a3487fb3ee6c60165fee9df92655a34c0d167654
johan <santiagohc123@gmail.com>
2020-12-12 19:06:24 -0600

    Emanuel_Metodos_Pago

---
+++ DeliveryCR_Project/Testing_Project/Accesos/Usuario/Acciones/FrmMetPago.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testing_Project.Accesos.Usuario.Acciones
+{
+    public partial class FrmMetPago : Form
+    {
+        public FrmMetPago()
+        {
+            InitializeComponent();
+        }
+    }
+}
